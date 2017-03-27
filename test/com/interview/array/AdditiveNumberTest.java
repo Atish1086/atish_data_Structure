d5675b7ea0fac699b7d516b32cea5ecba53c687f
Atish <atish.1084@gmail.com>
2017-03-28 00:43:24 +0530

    last changes/

---
+++ test/com/interview/array/AdditiveNumberTest.java
@@ -0,0 +1,12 @@
+package com.interview.array;
+
+import org.junit.Assert;
+
+public class AdditiveNumberTest {
+    public static void main(String args[]) {
+        AdditiveNumber additiveNumber = new AdditiveNumber();
+        Assert.assertTrue(additiveNumber.isAdditiveNumber("12351174"));
+       
+        
+    }
+}
